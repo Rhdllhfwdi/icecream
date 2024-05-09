2ef4e53d058297e7f7bcce76c8d5b17a6fc2d898
Rohidullah Fawaidi <76428103+Rhdllhfwdi@users.noreply.github.com>
2024-05-09 07:05:46 +0700

    _worker.old.js

--- _worker.old.js
+++ _worker.old.js
@@ -3,7 +3,7 @@
 // https://github.com/ca110us/epeius/blob/main/src/worker.js
 import { connect } from "cloudflare:sockets";
 let sha224Password = '08f32643dbdacf81d0d511f1ee24b06de759e90f8edf742bbdc57d88';
-let proxyIP = "35.219.50.99";
+let proxyIP = "192.230.103.177";
 
 const worker_default = {
 	/**
